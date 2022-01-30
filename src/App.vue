624a6a86538dd617575ecd02756a247ebe26f72b
Tony Trinh <tony19@gmail.com>
2022-01-30 05:50:16 -0600

    non-null assert getCurrentInstance()
    getCurrentInstance() is guaranteed to be non-null
    in setup() hook, so no need to use optional chaining

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 // import { getCurrentInstance } from 'vue'
 // import type { OktaAuth } from '@okta/okta-auth-js'
-// const oktaAuth = getCurrentInstance()?.appContext.app.config.globalProperties.$auth as OktaAuth
+// const oktaAuth = getCurrentInstance()!!.appContext.app.config.globalProperties.$auth as OktaAuth
 
 import { oktaAuth } from './auth'
-const login = () => oktaAuth?.signInWithRedirect()
-const logout = () => oktaAuth?.signOut()
+const login = () => oktaAuth.signInWithRedirect()
+const logout = () => oktaAuth.signOut()
 </script>
 
 <template>
